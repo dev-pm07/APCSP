d3169255a89c7d3a25aadd7c3f6025944c73315b
dev-pm07 <88402170+dev-pm07@users.noreply.github.com>
2022-10-19 21:49:54 -0500

    finished 1.1.8
    10/19 - home

--- 1.1.8/a118_turtles_in_traffic_pm.py
+++ 1.1.8/a118_turtles_in_traffic_pm.py
@@ -1,16 +1,24 @@
  #   a118_turtles_in_traffic.py
-#   Move turtles horizontally and vertically across screen.
+#   Move turtles horizontally and vertically across the screen.
 #   Stopping turtles when they collide.
 import turtle as trtl
 
-# create two empty lists of turtles, adding to them later
+# create two empty lists of turtles, adding to them later as well as two empty lists for the dead turtles
 horiz_turtles = []
 vert_turtles = []
+deadvt_turtles = []
+deadht_turtles = []
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle"]
 horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
+
+# Assign collision shape
+
+colShape = "classic"
+
+# Append colors to turtles and put in location
 
 tloc = 50
 for s in turtle_shapes:
@@ -33,27 +41,46 @@ for s in turtle_shapes:
   
   tloc += 50
 
-# hts = ['fastest', 'fast', 'normal', 'slow', 'slowest']
-# vts = ['fastest', 'fast', 'normal', 'slow', 'slowest']
+hts = ['fastest', 'fast', 'normal', 'slow']
+vts = ['fastest', 'fast', 'normal', 'slow']
 
 # TODO: move turtles across and down screen, stopping for collisions
 
 for step in range(50):
   for ht in horiz_turtles:
     for vt in vert_turtles:
-      # ht.speed(vts.pop())
-      # vt.speed(hts.pop())
+
+      if vt == 4:
+        vt.speed(vts.pop())
+        ht.speed(hts.pop())
+      elif vt == 8:
+        vt.speed(vts.pop())
+        ht.speed(hts.pop())
+      elif vt == 12:
+        vt.speed(vts.pop())
+        ht.speed(hts.pop())
+      elif vt == 32:
+        vt.speed(vts.pop())
+        ht.speed(hts.pop())
+
       vt.forward(3)
       ht.forward(3)
       if (abs(ht.xcor() - vt.xcor()) < 20):
         if (abs(ht.ycor() - vt.ycor()) < 20):
           ht.fillcolor("gray")
+          ht.shape(colShape)
+          deadht_turtles.append(ht)
           horiz_turtles.remove(ht) 
           vt.fillcolor("gray")
+          vt.shape(colShape)
+          deadvt_turtles.append(vt)
           vert_turtles.remove(vt)
+      
+
+for ht in deadht_turtles:
+  for vt in deadvt_turtles:
+    vt.fillcolor("maroon")
+    ht.fillcolor("maroon")
   
-
-
-
 wn = trtl.Screen()
 wn.mainloop()
