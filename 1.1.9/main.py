014a32484aaa27d91d63d0612b49ce259b579bfe
dev-pm07 <88402170+dev-pm07@users.noreply.github.com>
2022-11-01 16:10:35 -0500

    11/1 1.1.9 Finished
    :D

--- 1.1.9/main.py
+++ 1.1.9/main.py
@@ -1,26 +1,35 @@
 from turtle import Screen, Turtle
 import random as rand
 
+# Set colors for gradient
+
 COLOR = (0, .4118, .5804)  # (0% red, 41.18% green and 58.04% blue) top color
 TARGET = (.1804, .5451, .3412)  # (18.04% red, 54.51% green and 34.12% blue) bottom color
 
+# Init Screen
 
 wn = Screen()
 wn.tracer(False)
+
+turtle = Turtle()
 
 wn.setup(width = 1920, height = 1080)
 
 
 WIDTH, HEIGHT = wn.window_width(), wn.window_height()
 
+# Set deltas
+
 deltas = [(hue - COLOR[index]) / HEIGHT for index, hue in enumerate(TARGET)]
 
-turtle = Turtle()
+
 turtle.color(COLOR)
 
 turtle.penup()
 turtle.goto(-WIDTH, HEIGHT)#size1
 turtle.pendown()
+
+# Set direction and create gradient
 
 direction = 2
 
@@ -34,23 +43,23 @@ for distance, y in enumerate(range(HEIGHT//2, -HEIGHT//2, -1)):#size2
 
 wn.tracer(True)
 
+# Create organism turtle to create organisms
+
 organism = Turtle()
 
 organism.speed('fastest')
 
-# Set background color to sea
-wn.bgcolor('medium blue') 
+# Create starfish organism
 
 organism.pencolor('crimson')
 organism.penup()
-organism.goto(100, 0)
+organism.goto(200, -50)
 organism.pendown()
 
 head = 0
 organism.fillcolor('crimson')
 organism.begin_fill()
 
-# Create Starfish
 for i in range(5):
     head = head + 144
     organism.setheading(head)
@@ -58,11 +67,12 @@ for i in range(5):
 
 organism.end_fill()
 
+# Create top right barnacles in certain area and record each coordinate and compare to make sure they are not touching at least one barnacle
+
 topTakenCoords = []
 sizeOfBarnacles = 20
 numOfBarnacles = 25
 distBtwBarnacles = sizeOfBarnacles*2
-spawnSquare = 300
 numOfTakenCoords = 0
 takenXValues = []
 takenYValues = []
@@ -72,8 +82,8 @@ for i in range(numOfBarnacles):
     differenceXValues = []
     differenceYValues = []
 
-    xcoordinate = rand.randint(100, spawnSquare)
-    ycoordinate = rand.randint(100, spawnSquare)
+    xcoordinate = rand.randint(200, 500)
+    ycoordinate = rand.randint(200, 500)
 
     coordinate = [xcoordinate, ycoordinate]
     
@@ -87,13 +97,13 @@ for i in range(numOfBarnacles):
     
 
     if all(value < distBtwBarnacles for value in differenceXValues):
-        coordinate[0] = rand.randint(100, spawnSquare)
+        coordinate[0] = rand.randint(200, 500)
         check()
     else:
         pass
     
     if all(value < distBtwBarnacles for value in differenceYValues):
-        coordinate[1] = rand.randint(100, spawnSquare)
+        coordinate[1] = rand.randint(200, 500)
         check()
     else:
         pass
@@ -111,6 +121,8 @@ for i in range(numOfBarnacles):
     organism.circle(10)
     organism.end_fill()
 
+# Create bottom left barnacles in certain area and record each coordinate and compare to make sure they are not touching at least one barnacle
+
 bottomTakenCoords = []
 sizeOfBarnacles = 20
 numOfBarnacles = 25
@@ -124,8 +136,8 @@ for i in range(numOfBarnacles):
     differenceXValues = []
     differenceYValues = []
 
-    xcoordinate = rand.randint(-300, -100)
-    ycoordinate = rand.randint(-300, -100)
+    xcoordinate = rand.randint(-500, -200)
+    ycoordinate = rand.randint(-400, -200)
 
     coordinate = [xcoordinate, ycoordinate]
     
@@ -139,13 +151,13 @@ for i in range(numOfBarnacles):
     
 
     if all(value < distBtwBarnacles for value in differenceXValues):
-        coordinate[0] = rand.randint(-300, -100)
+        coordinate[0] = rand.randint(-500, -200)
         check()
     else:
         pass
     
     if all(value < distBtwBarnacles for value in differenceYValues):
-        coordinate[1] = rand.randint(-300, -100)
+        coordinate[1] = rand.randint(-400, -200)
         check()
     else:
         pass
@@ -166,6 +178,8 @@ for i in range(numOfBarnacles):
 barnacleCoords = topTakenCoords + bottomTakenCoords
 organism.hideturtle()
 
+# Pick barnacle coordinate on random and create bubble there
+
 bubble = Turtle()
 bubble.speed('slowest')
 
@@ -179,55 +193,27 @@ while True:
 
         def airBubble(bubble):  
         
-            # filling the color in the bubble  
             bubble.fillcolor('medium turquoise')  
-        
-            # starting to color the bubble  
             bubble.begin_fill()  
-        
-            # drawing the circle  
-            bubble.circle(bubbleSize)  
-        
-            # ending the color filling in the bubble  
+            bubble.circle(bubbleSize)
             bubble.end_fill()  
 
-        # setting the turtle object color to light sea green  
         bubble.color('light sea green')  
-
-        # setting the turtle object speed  
         bubble.speed(0)  
-
-        # setting the turtle object width  
         bubble.width(2)  
-
-        # hiding the turtle object  
         bubble.hideturtle()  
-
-        # turtle object in air  
+  
         bubble.penup()  
-
-        # # setting the initial position  
         bubble.goto(barnacleCoords[indexNum][0], barnacleCoords[indexNum][1])  
-
-        # moving turtle object to the surface  
         bubble.pendown() 
-
         bubble.speed('slowest')
 
         while bubble.ycor() < 450:  
 
-            # clearing the past location of bubble
             bubble.clear()  
-
-            # calling the method to draw the bubble  
             airBubble(bubble) 
-
             bubble.setheading(90)
-
-            # updating the screen  
             wn.update()  
-
-            # forward motion by turtle object  
             bubble.forward(5)  
         
         bubble.clear()
